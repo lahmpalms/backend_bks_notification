672f4959d2a8996f6787e2acfcda3f38462fd4cc
lahmpalms <swifttyg2@gmail.com>
2024-03-25 10:25:16 +0700

    fix: feat chat

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,23 +1,30 @@
-// import { wss } from "../utils/websocket.js";
+import { connectedClients, setUpWebSocketServer } from "../utils/websocket.js";
+import WebSocket from "ws";
 
-const handleChat = (req, res) => {
-  const { message, userId } = req.body;
+const handleChat = (req, res, next) => {
+  try {
+    const { message, userId } = req.body;
 
-  if (!message || !userId) {
-    return res
-      .status(400)
-      .send({ error: "Missing message or userId in request body" });
-  }
-
-  const newMessage = { userId, message, timestamp: new Date() };
-
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(newMessage));
+    if (!message || !userId) {
+      res.status(400).send({
+        code: 400,
+        message: "Missing message or userId in request body",
+      });
+      throw new Error("Missing message or userId in request body");
     }
-  });
 
-  res.status(200).send({ message: "Message sent successfully" });
+    const newMessage = { userId, message, timestamp: new Date() };
+
+    connectedClients.forEach(({ client }) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(newMessage));
+      }
+    });
+
+    res.status(200).send({ message: "Message sent successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default { handleChat };
